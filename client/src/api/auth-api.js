ee20c123c66ed7856abd896d396689ada08f081a
angelcheto <145483000+angelcheto@users.noreply.github.com>
2024-08-09 14:59:49 +0300

    Added register authentication

--- client/src/api/auth-api.js
+++ client/src/api/auth-api.js
@@ -8,7 +8,5 @@ const BASE_URL = 'http://localhost:3030/users';
  * @param {string} password  
  */
 
-export const login = async (email, password) => {
-    const authData = await requester.post(`${BASE_URL}/login`, {email, password});
-    return authData;
-};
+export const login = (email, password) =>  requester.post(`${BASE_URL}/login`, {email, password});
+export const register = (email, password) => requester.post(`${BASE_URL}/register`, {email, password});
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,25 +1,79 @@
+import { useNavigate } from "react-router-dom";
+import { useRegister } from "../../hooks/useAuth";
+import { useForm } from "../../hooks/useForm";
+import { useState } from "react";
+
+const initalValues = { email: '', password: '', conPassword: '' };
+
 export default function Register() {
+    const [error, setError] = useState('');
+    const register = useRegister();
+    const navigate = useNavigate();
+
+    const registerHandler = async (values) => {
+        if (values.password !== values.conPassword) {
+            return setError('Password does not match!');
+            
+        }
+
+        try {
+            await register(values.email, values.password)
+            navigate('/');
+        } catch (err) {
+            setError(err.message);
+        }
+    
+    };
+
+    const { values, changeHandler, submitHandler } = useForm(initalValues, registerHandler);
+
     return (
 
 
         <section id="register-page" className="content auth">
-            <form id="register">
+            <form id="register" onSubmit={submitHandler}>
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Register</h1>
 
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="maria@email.com"/>
+                    <input 
+                    type="email" 
+                    id="email" 
+                    name="email" 
+                    value={values.email}
+                    onChange={changeHandler
+
+                    }
+                    placeholder="maria@email.com"/>
 
                     <label htmlFor="pass">Password:</label>
-                    <input type="password" name="password" id="register-password"/>
+                    <input 
+                    type="password" 
+                    name="password" 
+                    id="register-password"
+                    value={values.password}
+                    onChange={changeHandler}
+                    />
 
                     <label htmlFor="con-pass">Confirm Password:</label>
-                    <input type="password" name="confirm-password" id="confirm-password"/>
+                    <input 
+                    type="password" 
+                    name="conPassword" 
+                    id="conPassword"
+                    value={values.conPassword}
+                    onChange={changeHandler}
+                    />
+
+                    {error && (
+                    <p className="field">
+                    <span>{error}</span>
+                    </p>                        
+                    )}
 
                     <input className="btn submit" type="submit" value="Register"/>
 
-                    <p className="field">
+                    <p>
                         <span>If you already have profile click <a href="#">here</a></span>
                     </p>
                 </div>
