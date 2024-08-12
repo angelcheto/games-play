fadbcdc6ad1c924039aeeceff3d86fe9d193c5b8
angelcheto <145483000+angelcheto@users.noreply.github.com>
2024-08-12 16:38:23 +0300

    Latest games on main menu added

--- client/src/api/games-api.js
+++ client/src/api/games-api.js
@@ -8,6 +8,17 @@ export const getAll = async () => {
     return games;
 };
 
+export const getLatest = async () => {
+    const urlSearchParams = new URLSearchParams({
+        sortBy: '_createdOn desc',
+        pageSize: 3,
+    });
+
+    const result = await request.get(`${BASE_URL}?${urlSearchParams.toString()}`);
+    const latestGames = Object.values(result);
+    return latestGames;
+};
+
 export const getOne = (gameId) => request.get(`${BASE_URL}/${gameId}`);
 
 export const create = (gameData) => request.post(`${BASE_URL}`, gameData);
@@ -18,6 +29,7 @@ export const update = (gameId, gameData) => request.put(`${BASE_URL}/${gameId}`,
 
 const gamesAPI = {
     getAll,
+    getLatest,
     getOne,
     create,
     remove,
