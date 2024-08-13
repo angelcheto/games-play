b8f55bad15c41ebced5dd84adcbf17fb0f181aea
angelcheto <145483000+angelcheto@users.noreply.github.com>
2024-08-13 22:55:04 +0300

    added logout navigate

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { login, register, logout } from "../api/auth-api";
 import { useAuthContext } from "../contexts/AuthContext"
 
@@ -29,10 +30,13 @@ export const useRegister = () => {
 
 export const useLogout = () => {
     const { logout: localLogout } = useAuthContext();
+    const navigate = useNavigate();
 
     const logoutHandler = async () => {
-        localLogout();
         await logout();
+        localLogout();
+        navigate("/login");
+        window.location.reload();
     };
 
     return logoutHandler;
